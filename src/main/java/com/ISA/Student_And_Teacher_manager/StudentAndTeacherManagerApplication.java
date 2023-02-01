337fee2324e048b75922786da12c32572d2e8953
chethika10 <chethika10@gmail.com>
2023-02-01 09:50:24 +0530

    first

---
+++ src/main/java/com/ISA/Student_And_Teacher_manager/StudentAndTeacherManagerApplication.java
@@ -0,0 +1,15 @@
+package com.ISA.Student_And_Teacher_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAndTeacherManagerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StudentAndTeacherManagerApplication.class, args);
+		System.out.println("assd");
+	}
+
+}
---
+++ src/test/java/com/ISA/Student_And_Teacher_manager/StudentAndTeacherManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ISA.Student_And_Teacher_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAndTeacherManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
